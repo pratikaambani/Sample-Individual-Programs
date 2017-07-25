99847cb5039ac50f3820c9392ab4cf1989fd9701
Pratik Ambani <pratikaambani@gmail.com>
2017-07-25 09:35:16 +0530

    First Commit

---
+++ src/main/java/InitialReferenceClasses001/SampleHelloWorld001.java
@@ -0,0 +1,58 @@
+package InitialReferenceClasses001;
+
+/**
+ * Created by VatshP2 on 23/05/2017.
+ */
+public class SampleHelloWorld001 implements I1{
+
+    public static int randomNumber = 10;
+
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+
+        SampleReference002 object = new SampleReference002();
+
+        System.out.println("This class Number value: " +randomNumber);
+        System.out.println("SampleReference002 Number value: " +object.randomNumber);
+        System.out.println("Reference Obj, Sum: " +object.sum(4,5));
+
+        for(long i=0;i<1000000000;i++) {
+            SampleReference002 object1 = new SampleReference002();
+            System.out.println(object1);
+        }
+    }
+
+    private long sum(int a, int b) {
+        return a+b;
+    }
+
+    public void print1() {
+        System.out.pri
+
+
+
+
+
+
+
+
+
+
+        ntln();
+
+    }
+
+    public void print2() {
+
+    }
+}
+
+class C3 extends SampleHelloWorld001 {
+
+}
+
+interface I1 {
+
+    public void print1();
+    public void print2();
+}
---
+++ src/main/java/InitialReferenceClasses001/SampleReference002.java
@@ -0,0 +1,14 @@
+package InitialReferenceClasses001;
+
+/**
+ * Created by VatshP2 on 23/05/2017.
+ */
+public class SampleReference002 {
+
+    public static int randomNumber = 100;
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+}
