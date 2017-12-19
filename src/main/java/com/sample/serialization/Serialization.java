a0878f3fd32697323eb83e4c754980c4a9368cd5
Pratik Ambani <pratikaambani@gmail.com>
2017-12-19 08:36:25 +0530

    updated serialization

---
+++ src/main/java/com/sample/serialization/Serialization.java
@@ -0,0 +1,26 @@
+package com.sample.serialization;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Serialization {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Pratik";
+        person.age = 24;
+        person.roll = 10;
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("FileinputStream.txt")) {
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(person);
+
+            System.out.println("Look serialVersionUID in Person Class");
+            System.out.printf("Great !! ");
+            System.out.println("Serialized data saved in FileinputStream.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
