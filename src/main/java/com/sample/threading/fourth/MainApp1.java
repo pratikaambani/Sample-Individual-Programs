9f671724dde1d4b7d9d1ea385d74a0d1e62ee4cb
Pratik Ambani <pratikaambani@gmail.com>
2018-02-17 22:28:37 +0530

    added threading examples

---
+++ src/main/java/com/sample/threading/fourth/MainApp1.java
@@ -0,0 +1,10 @@
+package com.sample.threading.fourth;
+
+/**
+ * Created by Pratik Ambani on 14/02/2018.
+ */
+public class MainApp1 {
+    public static void main(String[] args) {
+        new Worker().main();
+    }
+}
